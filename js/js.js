300cfd825f617ea949fb8f7c672ff025edd1458a
“Matteo git config --global user.email “matteo.soresini@hotmail.it <matteo.soresini@hotmail.it>
2016-09-22 11:24:25 +0200

    ..

--- js/js.js
+++ js/js.js
@@ -3,7 +3,13 @@ $(function() {
     $('#navbar-hamburger').toggleClass('hidden');
     $('#navbar-close').toggleClass('hidden');  
   });
-		$(".fancybox").fancybox();
+
+  $('.ss_button').on('click',function() {
+    $('.ss_content').slideUp('fast');
+    $(this).next('.ss_content').slideDown('fast');
+  });
+	
+	$(".fancybox").fancybox();
 
 
 	$(".fancybox").fancybox({
